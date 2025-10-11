59a3091b88051c80962ef179b32ce3d7af462932
Emmanuel <emmopk11@outlook.com>
2025-10-11 02:04:08 +0200

    Initial setup commit

---
+++ Restaurant/Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Emmanuel on 11.10.25.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
